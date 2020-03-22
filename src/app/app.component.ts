b7e41b33f82e18a5753ebd844c50afe2a19177ce
Damien Gallagher <damien.gallagher@gmail.com>
2020-03-22 10:11:30 +0000

    Update to tooltip being used

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,24 +17,25 @@ export class AppComponent implements OnInit {
               TaskID: '',
               TaskName: 'Project Initiation',
               StartDate: new Date('04/02/2019'),
-              EndDate: new Date('04/21/2019'),
-              subtasks: [
-                  {  TaskID: 2, TaskName: 'Identify Site location', StartDate: new Date('04/02/2019'), Duration: 4, Progress: 50 },
-                  { TaskID: 3, TaskName: 'Perform Soil test', StartDate: new Date('04/02/2019'), Duration: 4, Progress: 50  },
-                  { TaskID: 4, TaskName: 'Soil test approval', StartDate: new Date('04/02/2019'), Duration: 4, Progress: 50 },
-              ]
+              EndDate: new Date('04/21/2019')
           },
           {
               TaskID: 5,
               TaskName: 'Project Estimation',
               StartDate: new Date('04/02/2019'),
-              EndDate: new Date('04/21/2019'),
-              subtasks: [
+              EndDate: new Date('04/21/2019')
+              /*subtasks: [
                   { TaskID: 6, TaskName: 'Develop floor plan for estimation', StartDate: new Date('04/04/2019'), Duration: 3, Progress: 50 },
                   { TaskID: 7, TaskName: 'List materials', StartDate: new Date('04/04/2019'), Duration: 3, Progress: 50 },
                   { TaskID: 8, TaskName: 'Estimation approval', StartDate: new Date('04/04/2019'), Duration: 3, Progress: 50 }
-              ]
+              ]*/
           },
+          {
+            TaskID: '',
+            TaskName: 'Project Completion',
+            StartDate: new Date('03/03/2019'),
+            EndDate: new Date('05/05/2019')
+        },
       ];
       this.taskSettings = {
           id: 'TaskID',
@@ -42,7 +43,6 @@ export class AppComponent implements OnInit {
           startDate: 'StartDate',
           endDate: 'EndDate',
           duration: 'Duration',
-          progress: 'Progress',
           dependency: 'Predecessor',
           child: 'subtasks'
       };
